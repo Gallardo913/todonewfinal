68282b75959edcdbab2c130c0ce522b7eea3ff66
Roger gallardo <rogergallardofs1@gmail.com>
2022-12-19 23:44:30 -0500

    routes updated

--- routes/todoroute.js
+++ routes/todoroute.js
@@ -1,10 +1,14 @@
 const {Router} = require("express")
-const {getToDo, saveToDo} = require("../controllers/todocontroller");
+const {getToDo, saveToDo, updateToDo, deleteToDo} = require("../controllers/todocontroller");
 
 const router = Router()
 
 router.get('/', getToDo)
 
 router.post('/save', saveToDo)
+
+router.post('/update', updateToDo)
+
+router.post('/delete', deleteToDo)
 
 module.exports = router;
